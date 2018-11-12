8b86a2c0b7739f4bc499933f09e1903995515376
Tóth Csaba <>
2018-11-12 21:15:12 +0100

    0.1.1.

--- src/HTMLElementGenerator.js
+++ src/HTMLElementGenerator.js
@@ -1,7 +1,7 @@
 //Author: Csaba Tóth https://github.com/csabatibortoth/
 //LICENCE: MIT
 //Repository: https://github.com/csabatibortoth/HTMLElementGenerator
-//Version: 0.1.0
+//Version: 0.1.1
 
 function HTMLElementGenerator(options){
     if (options !== undefined) {
