92ebd24bd7d03d9add979439d5de69532648bd43
Tóth Csaba <>
2018-11-12 16:33:21 +0100

    Project start

---
+++ src/HTMLElementGenerator.js
@@ -0,0 +1,4 @@
+//Author: Csaba Tóth https://github.com/csabatibortoth/
+//LICENCE: MIT
+//Repository: https://github.com/csabatibortoth/HTMLElementGenerator
+//Version: 0.0.1
---
+++ src/addons/readme.txt
@@ -0,0 +1,1 @@
+This Directory is for the Addons of this Library.
